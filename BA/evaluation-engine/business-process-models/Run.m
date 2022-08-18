bc7fb4794a812b0608b269c8ef13192cbe2275c6
farided <94111797+farided@users.noreply.github.com>
2022-08-18 13:38:28 +0200

    Update Run.m

--- BA/evaluation-engine/business-process-models/Run.m
+++ BA/evaluation-engine/business-process-models/Run.m
@@ -6,9 +6,9 @@ x2 = webread("http://localhost:8080/w2");
 S2 = x2.AverageServiceTime;
 x3 = webread("http://localhost:8080/w3");
 S3 = x3.AverageServiceTime;
-S1 = 1;
-S2= 2;
-S3=3;
+%S1 = 1;
+%S2= 2;
+%S3=3;
 
 sim('CaseStudy_Simulink');
 
