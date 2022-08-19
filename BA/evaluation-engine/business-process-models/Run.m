6222eb2eb8b67e789df83719141897d35b3ca49f
farided <94111797+farided@users.noreply.github.com>
2022-08-19 10:59:05 +0200

    Update Run.m

--- BA/evaluation-engine/business-process-models/Run.m
+++ BA/evaluation-engine/business-process-models/Run.m
@@ -6,6 +6,8 @@ x2 = webread("http://localhost:8080/w2");
 S2 = x2.AverageServiceTime;
 x3 = webread("http://localhost:8080/w3");
 S3 = x3.AverageServiceTime;
+
+% for test:
 %S1 = 1;
 %S2= 2;
 %S3=3;
